62172df885fb7f442311724e3d9dddae46c3bfdb
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-22 18:31:55 +0000

    Builds ArticlesList to get and display articles data and filter by topic

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import NavBar from './NavBar';
+import { getArticles } from '../utils/api';
 
 export default function ArticlesList() {
-  return <div></div>;
+  const [topic, setTopic] = useState(undefined);
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    getArticles(topic).then((articles) => {
+      setArticles(articles);
+      setIsloading(false);
+    });
+  }, [topic]);
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <main>
+      <NavBar setTopic={setTopic} />
+      <div>
+        {articles.map((article) => {
+          return <p key={article.title}>{article.title}</p>;
+        })}
+      </div>
+    </main>
+  );
 }
