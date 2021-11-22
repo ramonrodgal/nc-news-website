f81d3c1803fe36dc8324b80a4dc4cce324540126
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-22 16:36:44 +0000

    Builds header title and link to home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import ArticlesList from './components/ArticlesList';
 import Article from './components/Article';
 import LogIn from './components/LogIn';
@@ -10,10 +11,11 @@ import User from './components/User';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<ArticlesList />} />
         <Route path="/articles" element={<ArticlesList />} />
-        <Route path='/articles/:topic' element={<ArticlesList />} />
+        <Route path="/articles/:topic" element={<ArticlesList />} />
         <Route path="/articles/:topic/:article_id" element={<Article />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <header>
+      <Link to="/">
+        <Typography variant="h1">NC News</Typography>
+      </Link>
+    </header>
+  );
 }
