bc593ab275fee5ad6463a6f6525f4992d57f2815
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-22 16:30:10 +0000

    Creates routing and basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import ArticlesList from './components/ArticlesList';
+import Article from './components/Article';
+import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
+import User from './components/User';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<ArticlesList />} />
+        <Route path="/articles" element={<ArticlesList />} />
+        <Route path='/articles/:topic' element={<ArticlesList />} />
+        <Route path="/articles/:topic/:article_id" element={<Article />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/users/:user_id" element={<User />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
