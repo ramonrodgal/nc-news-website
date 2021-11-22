003f9784188ff615b568d973f6ad4e97425cad2e
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-22 23:03:10 +0000

    displays article information

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { getArticleById } from '../utils/api';
 
 export default function Article() {
-  return <div></div>;
+  const [article, setArticle] = useState({});
+  const { article_id } = useParams();
+
+  useEffect(() => {
+    getArticleById(article_id).then((article) => {
+      setArticle(article);
+    });
+  }, []);
+
+  return (
+    <main>
+      <h2>{article.title}</h2>
+      <h3>{article.topic}</h3>
+      <p>
+        {article.author} - created at {article.created_at}
+      </p>
+      <p>{article.body}</p>
+      <p>Votes:{article.votes}</p>
+    </main>
+  );
 }
