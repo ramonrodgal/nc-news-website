ca816b5c7671960fe52f99edfe06c84e9b1be87d
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-23 09:26:09 +0000

    Creates 'Log In' and 'Sign Up' buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 export default function Header() {
   return (
     <header>
       <Link to="/">
         <Typography variant="h1">NC News</Typography>
+      </Link>
+      <Link to="/login">
+        <Button variant="contained">Log In</Button>
+      </Link>
+      <Link to="/signup">
+        <Button variant="contained">Sign Up</Button>
       </Link>
     </header>
   );
