8d9cda0c79a15c5a4b38503c3a2b4767dc8f1eec
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-23 12:28:12 +0000

    update Login component for conditional rendering

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -3,33 +3,32 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
+import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../contexts/UserContext.jsx';
 
 import { getUserByUsername } from '../utils/api';
 
 export default function LogIn() {
-  const { user, setUser } = useContext(UserContext);
+  const { setUser, isLoggedIn } = useContext(UserContext);
 
-  console.log(user, 'user');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const username = e.target[0].value;
 
-  return (
+    getUserByUsername(username)
+      .then((user) => {
+        setUser(user);
+      })
+      .catch((err) => {
+        console.log(err.msg);
+      });
+  };
+
+  return !isLoggedIn ? (
     <main>
       <Typography variant="h2">Log In</Typography>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const username = e.target[0].value;
-
-          getUserByUsername(username)
-            .then((user) => {
-              setUser(user);
-            })
-            .catch((err) => {
-              console.log(err.msg);
-            });
-        }}
-      >
+      <form onSubmit={handleSubmit}>
         <FormControl>
           <TextField label="Username" variant="outlined" required></TextField>
           <Button variant="contained" type="submit">
@@ -37,6 +36,13 @@ export default function LogIn() {
           </Button>
         </FormControl>
       </form>
+    </main>
+  ) : (
+    <main>
+      <h2>You are logged in!</h2>
+      <Link to="/">
+        <Button>Go to home page</Button>
+      </Link>
     </main>
   );
 }
