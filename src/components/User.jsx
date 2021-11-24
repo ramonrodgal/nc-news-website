ffc25c5a80dc4e8e1581e09751a1b9a77b56110b
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-24 11:41:32 +0000

    Fetch and display user data

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getUserByUsername } from '../utils/api';
+
+import Avatar from '@mui/material/Avatar';
 
 export default function User() {
-  return <div></div>;
+  const { username } = useParams();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    getUserByUsername(username)
+      .then((user) => {
+        setUser(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Avatar alt={user.name} src={user.avatar_url} />
+      <h2>{user.username}</h2>
+    </div>
+  );
 }
