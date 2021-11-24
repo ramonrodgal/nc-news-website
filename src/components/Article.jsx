d79ca17eb37a2430419a2209f0dccae2f6b9f6f3
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-24 17:26:19 +0000

    Adds vote system to articles

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,19 +1,39 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import { getArticleById } from '../utils/api';
+import { getArticleById, updateArticleVotes } from '../utils/api';
 import Comments from '../components/Comments';
+import { UserContext } from '../contexts/UserContext';
+
+import Button from '@mui/material/Button';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
 export default function Article() {
   const { article_id } = useParams();
+  const { isLoggedIn } = useContext(UserContext);
+
   const [article, setArticle] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [votes, setVotes] = useState(0);
 
   useEffect(() => {
     getArticleById(article_id).then((article) => {
       setArticle(article);
       setIsLoading(false);
+      setVotes(article.votes);
     });
   }, [article_id]);
+
+  const handleVote = (vote) => {
+    setVotes((prevVotes) => prevVotes + vote);
+    updateArticleVotes(article_id, vote)
+      .then((article) => {
+        console.log(article.votes);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   if (isLoading)
     return (
@@ -30,7 +50,18 @@ export default function Article() {
         {article.author} - created at {article.created_at}
       </p>
       <p>{article.body}</p>
-      <p>Votes:{article.votes}</p>
+      <p>Votes:{votes}</p>
+      {isLoggedIn === true ? (
+        <>
+          <Button onClick={() => handleVote(1)}>
+            <ArrowUpwardIcon />
+          </Button>
+          <Button onClick={() => handleVote(-1)}>
+            <ArrowDownwardIcon />
+          </Button>{' '}
+        </>
+      ) : null}
+
       <Comments article_id={article_id} articleAuthor={article.author} />
     </main>
   );
