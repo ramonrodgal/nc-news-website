1f789cca8c94efe8e739d9dadd24a420a3ef11fe
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-24 11:13:06 +0000

    displays user info when logged in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
+import { Link } from 'react-router-dom';
+
 export default function Header() {
+  const { user, isLoggedIn, setUser } = useContext(UserContext);
+
+  console.log(isLoggedIn);
+
   return (
     <header>
       <Link to="/">
         <Typography variant="h1">NC News</Typography>
       </Link>
-      <Link to="/login">
-        <Button variant="contained">Log In</Button>
-      </Link>
-      <Link to="/signup">
-        <Button variant="contained">Sign Up</Button>
-      </Link>
+      {isLoggedIn ? (
+        <>
+          <Avatar alt={user.name} src={user.avatar_url}></Avatar>
+          <p>{user.username}</p>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setUser({});
+            }}
+          >
+            logout
+          </Button>
+        </>
+      ) : (
+        <>
+          <Link to="/login">
+            <Button variant="contained">Log In</Button>
+          </Link>
+          <Link to="/signup">
+            <Button variant="contained">Sign Up</Button>
+          </Link>
+        </>
+      )}
     </header>
   );
 }
