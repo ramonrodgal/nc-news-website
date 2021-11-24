de28cbd025459c73c59b3af5a412b1cb649a1849
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-24 23:24:32 +0000

    adds form to add comment to an article

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,10 +1,19 @@
-import { useEffect, useState } from 'react';
-import { getComments } from '../utils/api';
+import { useEffect, useState, useContext } from 'react';
+import { getComments, postComment } from '../utils/api';
 import Comment from './Comment';
+
+import { UserContext } from '../contexts/UserContext';
+
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
 
 export default function Comments({ article_id, articleAuthor }) {
   const [comments, setComments] = useState([]);
   const [isLoading, setIsloading] = useState(true);
+  const [newComment, setNewComment] = useState('');
+  const { user } = useContext(UserContext);
 
   useEffect(() => {
     getComments(article_id)
@@ -17,11 +26,42 @@ export default function Comments({ article_id, articleAuthor }) {
       });
   }, [article_id]);
 
+  const handlePostComment = () => {
+    const body = {
+      username: user.username,
+      body: newComment,
+    };
+    postComment(article_id, body)
+      .then((comment) => {
+        setComments([comment, ...comments]);
+      })
+      .catch((err) => {
+        console.lod(err);
+      });
+  };
+
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <div>
       <h2>Comments</h2>
+      <FormControl>
+        <TextField
+          id="outlined-textarea"
+          label="Write a comment"
+          multiline
+          onBlur={(e) => {
+            setNewComment(e.target.value);
+          }}
+        />
+        <Button
+          onClick={handlePostComment}
+          variant="contained"
+          endIcon={<SendIcon />}
+        >
+          Send
+        </Button>
+      </FormControl>
       {comments.map((comment) => {
         return (
           <Comment
