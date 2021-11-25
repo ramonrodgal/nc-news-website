480c403778af5afbacedcdeba2b19dab1dfe0eb3
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-25 21:22:45 +0000

    Creates component for page not found

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
 import User from './components/User';
 import Footer from './components/Footer';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
+        <Route path="*" element={<NotFound />}></Route>
         <Route path="/" element={<ArticlesList />} />
         <Route path="/articles" element={<ArticlesList />} />
         <Route path="/articles/:topic" element={<ArticlesList />} />
