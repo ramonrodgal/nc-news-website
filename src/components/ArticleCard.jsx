d087f96dad3953cf3f6e317d1e8d435a07e4b575
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-25 00:10:06 +0000

    displays author avatar in article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { getUserByUsername } from '../utils/api';
+import Avatar from '@mui/material/Avatar';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function ArticleCard({ article }) {
+  const [author, setAuthor] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getUserByUsername(article.author)
+      .then((author) => {
+        setAuthor(author);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [article.author]);
+
+  if (isLoading)
+    return (
+      <div>
+        <CircularProgress />
+      </div>
+    );
+
   return (
     <div>
       <h2>{article.title}</h2>
+      <Avatar alt={author.name} src={author.avatar_url} />
       <p>
         {article.author} - {article.created_at}
       </p>
