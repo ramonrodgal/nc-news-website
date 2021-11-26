76e67acc9c90a6ae3899aa0e4fd84b291338a127
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-11-26 08:35:43 +0000

    Use grid system in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,9 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
 import { useContext } from 'react';
 import { UserContext } from '../contexts/UserContext';
 import { Link } from 'react-router-dom';
@@ -11,34 +14,37 @@ export default function Header() {
 
   return (
     <header>
-      <Link to="/">
-        <Typography variant="h1">NC News</Typography>
-      </Link>
-      {isLoggedIn ? (
-        <>
-          <Link to={`/users/${user.username}`}>
-            <Avatar alt={user.name} src={user.avatar_url}></Avatar>
-          </Link>
-          <p>{user.username}</p>
-          <Button
-            variant="contained"
-            onClick={() => {
-              setUser({});
-            }}
-          >
-            logout
-          </Button>
-        </>
-      ) : (
-        <>
-          <Link to="/login">
-            <Button variant="contained">Log In</Button>
-          </Link>
-          {/* <Link to="/signup">
-            <Button variant="contained">Sign Up</Button>
-          </Link> */}
-        </>
-      )}
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container>
+          <Grid item xs={10}>
+            <Link to="/">
+              <Typography variant="h1">NC News</Typography>
+            </Link>
+          </Grid>
+          <Grid item xs={2}>
+            {isLoggedIn ? (
+              <>
+                <Link to={`/users/${user.username}`}>
+                  <Avatar alt={user.name} src={user.avatar_url}></Avatar>
+                </Link>
+                <p>{user.username}</p>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    setUser({});
+                  }}
+                >
+                  logout
+                </Button>
+              </>
+            ) : (
+              <Link to="/login">
+                <Button variant="contained">Log In</Button>
+              </Link>
+            )}
+          </Grid>
+        </Grid>
+      </Box>
     </header>
   );
 }
