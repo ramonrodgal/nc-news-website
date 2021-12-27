52c396f734abe8b6e19ee4b802d0b1b13ca59660
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-12-27 22:00:28 +0000

    Improve NotFound page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import Typography from "@mui/material/Typography";
+import BugReportIcon from "@mui/icons-material/BugReport";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Page Not Found :(</h2>
-    </div>
+    <>
+      <Typography variant="div">
+        <Typography variant="h2">
+          <BugReportIcon /> OOPS!
+        </Typography>
+        <Typography variant="h3">Error 404: Page Not Found :(</Typography>
+      </Typography>
+      <Typography variant="div">
+        <Typography variant="h4">Please check the URL.</Typography>
+        <Typography variant="h4">
+          Otherwise, <Link to="/">click here</Link> to be redirected to the
+          homepage.
+        </Typography>
+      </Typography>
+    </>
   );
 }
