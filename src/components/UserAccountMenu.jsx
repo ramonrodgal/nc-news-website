ae31aa7a2e5f9bb51992a0aee00309a9636687e9
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-03 23:38:53 +0000

    Change size of user Avatar in UserAccountMenu component

--- src/components/UserAccountMenu.jsx
+++ src/components/UserAccountMenu.jsx
@@ -1,15 +1,15 @@
-import { useContext, useState } from 'react';
-import { UserContext } from '../contexts/UserContext';
-import { Link } from 'react-router-dom';
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { Link } from "react-router-dom";
 
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import Logout from '@mui/icons-material/Logout';
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import Logout from "@mui/icons-material/Logout";
 
 export default function UserAccountMenu() {
   const { user, setUser } = useContext(UserContext);
@@ -27,7 +27,11 @@ export default function UserAccountMenu() {
     <>
       <Tooltip title="Account settings">
         <IconButton onClick={handleClick}>
-          <Avatar alt={user.name} src={user.avatar_url}></Avatar>
+          <Avatar
+            alt={user.name}
+            src={user.avatar_url}
+            sx={{ width: 56, height: 56 }}
+          ></Avatar>
         </IconButton>
       </Tooltip>
       <Menu
@@ -38,31 +42,31 @@ export default function UserAccountMenu() {
         PaperProps={{
           elevation: 0,
           sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            '& .MuiAvatar-root': {
+            "& .MuiAvatar-root": {
               width: 32,
               height: 32,
               ml: -0.5,
               mr: 1,
             },
-            '&:before': {
+            "&:before": {
               content: '""',
-              display: 'block',
-              position: 'absolute',
+              display: "block",
+              position: "absolute",
               top: 0,
               right: 14,
               width: 10,
               height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
               zIndex: 0,
             },
           },
         }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
         <Link to={`/users/${user.username}`}>
           <MenuItem>
