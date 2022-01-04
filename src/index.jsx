3869d1c0af54d8ed9245f6206aaec38de36e2e6f
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-04 15:35:29 +0000

    Move index.css to styles folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './contexts/UserContext';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./contexts/UserContext";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,5 +13,5 @@ ReactDOM.render(
       </BrowserRouter>
     </UserProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
